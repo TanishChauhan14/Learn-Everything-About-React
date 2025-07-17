7ed4831eeb75bce56c1337b4e85e1ecb3c076b2c
Tanish Chauhan <tanishrajput@gmail.com>
2025-07-17 10:44:51 +0530

    learning compulsory rulein react + vite and react.

--- 01vitebasicreact/src/App.jsx
+++ 01vitebasicreact/src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+    <p>1.function and file name first letter should be in uppercase. </p>
+    <p>2. Vite m html m direct import krte hai script ko. </p>
+    <p>3.React projtct m react-script use hoti hai script ko use krne ke liye.</p>
     </>
   )
 }
